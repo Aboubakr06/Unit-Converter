b0f37200537414525ff9abe34be925fd6c358970
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2022-12-13 15:28:48 +0100

    commit

--- script.js
+++ script.js
@@ -17,22 +17,22 @@ buttonCv.addEventListener("click", function() {
 let result1 = ""
 let result2 = ""
 function length() {
-    result1 = (inputEl.value * 3.281).toFixed(3)
-    result2 = (inputEl.value / 3.281).toFixed(3)
-    lengthRs.innerHTML = `${inputEl.value} meters = ${result1} feet | ${inputEl.value} feet = ${result2} meters` 
+    result1 = (inputEl.value * 3.280839895)
+    result2 = (inputEl.value / 3.280839895)
+    lengthRs.innerHTML = `${inputEl.value} meters = ${result1.toFixed(3)} feet | ${inputEl.value} feet = ${result2.toFixed(3)} meters` 
     
 }
 
 function volume() {
-    result1 = (inputEl.value * 0.264).toFixed(3)
-    result2 = (inputEl.value / 0.264).toFixed(3)
-    volumeRs.innerHTML = `${inputEl.value} liters = ${result1} gallons | ${inputEl.value} gallons = ${result2} liters` 
+    result1 = (inputEl.value * 0.2641720524)
+    result2 = (inputEl.value / 0.2641720524)
+    volumeRs.innerHTML = `${inputEl.value} liters = ${result1.toFixed(3)} gallons | ${inputEl.value} gallons = ${result2.toFixed(3)} liters` 
     
 }
 
 function mass() {
-    result1 = (inputEl.value * 2.204).toFixed(3)
-    result2 = (inputEl.value / 2.204).toFixed(3)
-    massRs.innerHTML = `${inputEl.value} kilos = ${result1} pounds | ${inputEl.value} pounds = ${result2} kilos` 
+    result1 = (inputEl.value * 2.2046226218)
+    result2 = (inputEl.value / 2.2046226218)
+    massRs.innerHTML = `${inputEl.value} kilos = ${result1.toFixed(3)} pounds | ${inputEl.value} pounds = ${result2.toFixed(3)} kilos` 
     
 }
